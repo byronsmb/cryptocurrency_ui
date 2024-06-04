896319b3f560487a46dda1731175f8dfe37c2096
Byron Montaño <byronmb0407@gmail.com>
2024-06-03 22:52:50 -0500

    actualizando

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -5,6 +5,25 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3, // number of items in each row
+        mainAxisSpacing: 8.0, // spacing between rows
+        crossAxisSpacing: 8.0, // spacing between columns
+      ),
+      padding: EdgeInsets.all(8.0), // padding around the grid
+      itemCount: 4, // total number of items
+      itemBuilder: (context, index) {
+        return Container(
+          color: Colors.blue, // color of grid items
+          child: Center(
+            child: Text(
+              "hola",
+              style: TextStyle(fontSize: 18.0, color: Colors.white),
+            ),
+          ),
+        );
+      },
+    );
   }
 }
