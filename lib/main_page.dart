2087860aca6bc9c1681b9daf96d2fa3bccef33ff
Byron Montaño <byronmb0407@gmail.com>
2024-06-03 22:55:28 -0500

    Update main_page.dart

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -5,25 +5,28 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3, // number of items in each row
-        mainAxisSpacing: 8.0, // spacing between rows
-        crossAxisSpacing: 8.0, // spacing between columns
-      ),
-      padding: EdgeInsets.all(8.0), // padding around the grid
-      itemCount: 4, // total number of items
-      itemBuilder: (context, index) {
-        return Container(
-          color: Colors.blue, // color of grid items
-          child: Center(
-            child: Text(
-              "hola",
-              style: TextStyle(fontSize: 18.0, color: Colors.white),
+    return Scaffold(
+      appBar: AppBar(title: Text("Home")),
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3, // number of items in each row
+          mainAxisSpacing: 8.0, // spacing between rows
+          crossAxisSpacing: 8.0, // spacing between columns
+        ),
+        padding: EdgeInsets.all(8.0), // padding around the grid
+        itemCount: 4, // total number of items
+        itemBuilder: (context, index) {
+          return Container(
+            color: Colors.blue, // color of grid items
+            child: Center(
+              child: Text(
+                "hola",
+                style: TextStyle(fontSize: 18.0, color: Colors.white),
+              ),
             ),
-          ),
-        );
-      },
+          );
+        },
+      ),
     );
   }
 }
