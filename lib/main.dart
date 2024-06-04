7a4d91cad2ed1f86547602473aec11f8b9959312
Byron Montaño <byronmb0407@gmail.com>
2024-06-03 22:59:54 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Cryptocurrency UI',
       theme: ThemeData(
+        brightness: Brightness.dark,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
